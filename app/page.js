c23b7a33d8097d85205c03905771899c26a94c90
muasyamuthengi-gif <muasyamuthengi@gmail.com>
2025-12-15 10:37:47 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-serif">
+        Aesthetic Finds by Anna
+      </h1>
+    </main>
+  );
+}
