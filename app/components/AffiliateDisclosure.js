e2e7b37e891ae88277ec5fd0740c651a226c13fd
muasyamuthengi-gif <muasyamuthengi@gmail.com>
2026-01-02 15:35:41 +0300

    Add AffiliateDisclosure component with affiliate notice

---
+++ app/components/AffiliateDisclosure.js
@@ -0,0 +1,7 @@
+export default function AffiliateDisclosure() {
+  return (
+    <p className="text-sm text-black-500 mb-8">
+      This post contains affiliate links. I may earn a small commission at no extra cost to you.
+    </p>
+  );
+}
