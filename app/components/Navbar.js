dab74a7ed1eb232ed4af0f6ecf6079de5c6cd824
muasyamuthengi-gif <muasyamuthengi@gmail.com>
2026-01-10 16:33:49 +0300

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -30,7 +30,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-6 py-6 flex justify-between items-center">
         
         {/* Logo */}
-        <Link href="/" className="text-xl font-serif">
+        <Link href="/" className="text-2xl font-serif font-bold">
           Aesthetic Finds
         </Link>
 
